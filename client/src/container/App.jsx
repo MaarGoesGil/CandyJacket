5b7c4ba1e331c87d3c1a1d21abfb804072e01645
MaarGoesGil <abrilmartinagil@gmail.com>
2022-02-03 17:09:58 -0300

    start to projects

---
+++ client/src/container/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Candy Jacket</h1>
+      <p>Proximamente...</p>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/container/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import axios from 'axios'
+import {dotenv} from 'dotenv'
+dotenv.config()
+
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
